80b3288d3f6eb29b96df15fa008c85407688611e
Sergio Vargas <sergio@parpadeo.co>
2016-04-16 22:08:41 -0500

    Initial Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    Transform player;
+    public float camSpeed = 5;
+    Vector3 temp;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent("Transform") as Transform;
+        temp = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        temp = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+
+        transform.position = Vector3.Slerp(transform.position, temp, camSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float playerSpeed = 5;
+    public float fuerzaEmpujon = 5;
+    public float actualSize = 1;
+    public int fishesEaten = 0;
+    public Vector3 lastCheckPoint, initialPoint;
+
+    public Sprite actualSprite;
+
+	void Start () {
+
+        initialPoint = transform.position;
+        lastCheckPoint = transform.position;
+	}
+	
+	void Update () {
+
+        //Movimiento Horizontal
+        transform.Translate(Vector3.right * playerSpeed * Time.deltaTime);
+
+        //Input
+        if (DetectInput())
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * fuerzaEmpujon * Time.deltaTime);
+	}
+
+    bool DetectInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            return true;
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            return true;
+        if (Input.GetMouseButtonDown(0))
+            return true;
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+            return true;
+
+        return false;
+    }
+
+    void ResetAll()
+    {
+        Debug.Log("Toco el nivel");
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Level"))
+            ResetAll();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("CheckPoint"))
+        {
+
+        }
+    }
+}
